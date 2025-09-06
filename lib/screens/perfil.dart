adac1031b10e555509dd6cfac89bcb943300f7df
Marco Antonio Arantes <142439630+maarantes@users.noreply.github.com>
2025-09-06 18:22:43 -0300

    [MERGE] feature: #25 Adicionando navbar e sistema de permissões

--- lib/screens/perfil.dart
+++ lib/screens/perfil.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sistema_almox/core/theme/colors.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +14,7 @@ class HomeScreen extends StatelessWidget {
 
       child: const Center(
         child: Text(
-          'Página Inicial',
+          'Página Perfil',
           style: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w600,
---
+++ lib/widgets/main_scaffold.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:sistema_almox/core/theme/colors.dart';
+
+import '../config/permissions.dart';
+import '../services/user_service.dart';
+
+import '../screens/home.dart';
+import '../screens/estoque.dart';
+import '../screens/admin.dart';
+import '../screens/pedidos.dart';
+import '../screens/perfil.dart';
+
+class _NavBarItemInfo {
+  final String imagePath;
+  final String label;
+  final int index;
+
+  _NavBarItemInfo(this.imagePath, this.label, this.index);
+}
+
+class MainScaffold extends StatefulWidget {
+  const MainScaffold({super.key});
+
+  @override
+  State<MainScaffold> createState() => _MainScaffoldState();
+}
+
+class _MainScaffoldState extends State<MainScaffold> {
+  int _selectedIndex = 0;
+
+  late final List<Widget> _pages;
+  late final List<_NavBarItemInfo> _navBarItemsInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    _buildNavigationLists();
+  }
+
+  void _buildNavigationLists() {
+    final pages = <Widget>[];
+    final navBarItemsInfo = <_NavBarItemInfo>[];
+
+    pages.add(const HomeScreen());
+    navBarItemsInfo.add(
+      _NavBarItemInfo(
+        'assets/icons/navbar/home.svg',
+        'Início',
+        navBarItemsInfo.length,
+      ),
+    );
+
+    pages.add(const StockScreen());
+    navBarItemsInfo.add(
+      _NavBarItemInfo(
+        'assets/icons/navbar/estoque.svg',
+        'Estoque',
+        navBarItemsInfo.length,
+      ),
+    );
+
+    if (UserService.instance.can(AppPermission.accessAdminScreen)) {
+      pages.add(const AdminScreen());
+      navBarItemsInfo.add(
+        _NavBarItemInfo(
+          'assets/icons/navbar/admin.svg',
+          'Admin',
+          navBarItemsInfo.length,
+        ),
+      );
+    }
+
+    pages.add(const OrderScreen());
+    navBarItemsInfo.add(
+      _NavBarItemInfo(
+        'assets/icons/navbar/pedidos.svg',
+        'Pedidos',
+        navBarItemsInfo.length,
+      ),
+    );
+
+    pages.add(const ProfileScreen());
+    navBarItemsInfo.add(
+      _NavBarItemInfo(
+        'assets/icons/navbar/perfil.svg',
+        'Perfil',
+        navBarItemsInfo.length,
+      ),
+    );
+
+    _pages = pages;
+    _navBarItemsInfo = navBarItemsInfo;
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages.elementAt(_selectedIndex),
+      bottomNavigationBar: _buildCustomNavBar(),
+    );
+  }
+
+  Widget _buildCustomNavBar() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withAlpha(24),
+            offset: const Offset(0, -1),
+            blurRadius: 8,
+            spreadRadius: 0,
+          ),
+        ],
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: _navBarItemsInfo.map((item) {
+              return _buildNavItem(item.imagePath, item.label, item.index);
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(String imagePath, String label, int index) {
+    final bool isSelected = _selectedIndex == index;
+    final Color iconColor = isSelected ? brandBlue : text40;
+
+    return Expanded(
+      child: InkWell(
+        onTap: () => _onItemTapped(index),
+        borderRadius: BorderRadius.circular(20),
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+              decoration: BoxDecoration(
+                color: isSelected ? brandBlueLight : Colors.transparent,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: SvgPicture.asset(
+                imagePath,
+                width: 26,
+                height: 26,
+                colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              label,
+              style: TextStyle(
+                color: isSelected ? brandBlue : text40,
+                fontWeight: FontWeight.w700,
+                fontSize: 12,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
