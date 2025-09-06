868c6ff7246aaf25848348e400438098fb609cb0
Marco Antonio <marcoantoniopou@gmail.com>
2025-09-06 18:15:37 -0300

    feature: #25 Adicionando props ao componente Botão e criando componente Snackbar

---
+++ lib/widgets/snackbar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:sistema_almox/core/theme/colors.dart';
+
+void showCustomSnackbar(
+  BuildContext context,
+  String message, {
+  bool isError = false,
+}) {
+  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+  final Color backgroundColor = isError ? deleteRed : brandBlue;
+
+  final snackBar = SnackBar(
+    content: Text(
+      message,
+      style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+    ),
+    backgroundColor: backgroundColor,
+    padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+  );
+
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
