9c7abc8a37729d990655759df315ad6c7ab4e889
Eber Junior <eberssjunior12@gmail.com>
2025-09-12 11:42:19 -0300

    feat: #34 Adicionando widget de input (texto, calendario e apenas números)

--- lib/screens/novopedido.dart
+++ lib/screens/novopedido.dart
@@ -1,22 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sistema_almox/core/theme/colors.dart';
+import 'package:sistema_almox/widgets/imputgeral.dart';
 
 class NewOrderScreen extends StatelessWidget {
   const NewOrderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      width: double.infinity,
-      height: double.infinity,
-      child: const Center(
-        child: Text(
-          'Página Novo Pedido',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
-            color: text40,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Página Novo Pedido',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: text40,
+                ),
+              ),
+              const SizedBox(height: 24),
+              CustomInput(
+                hintText: 'Digite algo',
+                iconPath: 'assets/icons/search.svg', // Exemplo de ícone SVG
+              ),
+              const SizedBox(height: 16),
+              CustomInput(
+                isCalendarMode: true,
+                onDateSelected: (date) {
+                  print('Data selecionada: $date');
+                },
+              ),
+              const SizedBox(height: 16),
+              CustomInput(
+                hintText: 'Apenas números',
+                onlyNumbers: true,
+                iconPath: 'assets/icons/number.svg', // Exemplo de ícone SVG
+              ),
+            ],
           ),
         ),
       ),
