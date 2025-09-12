e0ced67a36a28f0c38ab6dd92cdc7356102b0772
Eber Junior <eberssjunior12@gmail.com>
2025-09-12 08:43:52 -0300

    feat: #34 Adicionando rota de criar novo pedido

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'screens/login.dart';
-import 'widgets/main_scaffold.dart'; 
+import 'screens/novopedido.dart';
+import 'widgets/main_scaffold.dart';
 
 class AppRoutes {
   static const String login = '/';
   static const String home = '/home';
+  static const String newOrder = '/novopedido';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case login:
         return MaterialPageRoute(builder: (_) => const Login());
-      
+
       case home:
         return MaterialPageRoute(builder: (_) => const MainScaffold());
+
+      case newOrder:
+        return MaterialPageRoute(builder: (_) => const NewOrderScreen());
 
       default:
         return MaterialPageRoute(
