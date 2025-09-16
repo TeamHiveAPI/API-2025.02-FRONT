cd2af933165e04bb0c0eceeb75e1b3282a8e1e53
gabriel santos <gabriel.felipesantos22@gmail.com>
2025-09-16 09:46:19 -0300

    #26 adicionando modal de novo_item

--- lib/screens/estoque.dart
+++ lib/screens/estoque.dart
@@ -4,6 +4,7 @@ import 'package:sistema_almox/core/theme/colors.dart';
 import 'package:sistema_almox/widgets/button.dart';
 import 'package:sistema_almox/widgets/data_table/content/stock_list.dart';
 import 'package:sistema_almox/widgets/inputs/search.dart';
+import 'package:sistema_almox/widgets/modal/content/novo_item.dart';
 
 class StockScreen extends StatefulWidget {
   const StockScreen({super.key});
@@ -35,9 +36,10 @@ class _StockScreenState extends State<StockScreen> {
               text: 'Adicionar novo item',
               icon: Icons.add,
               widthPercent: 1.0,
-              onPressed: () {},
+              onPressed: () => showNovoItemModal(context),
             ),
             const SizedBox(height: 24),
+
 
             const Text(
               'Listagem do Inventário',
