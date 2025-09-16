75ca9d1e200cf7b7d4eae37c72a0e61f1d15a1ab
Marco Antonio <marcoantoniopou@gmail.com>
2025-09-16 12:15:13 -0300

    feature: #37 Adicionando visualização de tipos de medicamentos na tela Estoque

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -14,7 +14,7 @@ class AuthService {
       await Future.delayed(const Duration(seconds: 1)); 
 
       // Simular que o usuário que entrou é um coronel
-      final userRoleFromAPI = UserRole.tenenteFarmacia;
+      final userRoleFromAPI = UserRole.coronel;
 
       UserService.instance.login(userRoleFromAPI);
       
