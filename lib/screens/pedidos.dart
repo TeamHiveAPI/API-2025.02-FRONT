f22852acad026394e67ab5b07e26d0dc836c9380
gabriel santos <gabriel.felipesantos22@gmail.com>
2025-09-23 09:47:43 -0300

    feature: #35 adicionando a barra de pesquisa e adicionando botões de rota para novo pedido e historico

--- lib/screens/pedidos.dart
+++ lib/screens/pedidos.dart
@@ -1,26 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:sistema_almox/config/permissions.dart';
 import 'package:sistema_almox/core/theme/colors.dart';
+import 'package:sistema_almox/widgets/button.dart';
+import 'package:sistema_almox/widgets/inputs/search.dart';
 import 'package:sistema_almox/widgets/data_table/content/pedidos_list.dart';
-class OrderScreen extends StatelessWidget {
+
+class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key});
+
+  @override
+  State<OrderScreen> createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  String _searchQuery = '';
+  final UserRole _currentUserRole = UserRole.coronel;
+
+  void _handleSearch(String query) {
+    setState(() {
+      _searchQuery = query;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Text(
-            'Página Pedidos',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-          ),
-          Expanded(
-            child: PedidosTable(
-              searchQuery: '', 
-              userRole: UserRole.coronel,
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomButton(
+              text: 'Registrar Novo Pedido',
+              icon: Icons.add,
+              widthPercent: 1.0,
+              onPressed: () {
+                Navigator.pushNamed(context, '/novo-pedido');
+              },
             ),
-          ),
-        ],
+            const SizedBox(height: 16),
+
+             SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  // ação para histórico
+                },
+                label: const Text('Meu Histórico de Pedidos'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF7F9FF), 
+                  foregroundColor: const Color(0xFF2847AE), 
+                  elevation: 0,
+                  padding: const EdgeInsets.symmetric(vertical: 14),
+                  textStyle: const TextStyle(fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+
+            const Text(
+              'Listagem de Pedidos do Sistema',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: text40,
+              ),
+            ),
+            const SizedBox(height: 16),
+            const SizedBox(height: 20),
+
+            PedidosTable(
+              searchQuery: _searchQuery,
+              userRole: _currentUserRole,
+            ),
+          ],
+        ),
       ),
     );
   }
